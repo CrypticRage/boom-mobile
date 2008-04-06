dcb687daeeefc8a0636d48f3d47a098ada93edbf
ksu.deadeye <ksu.deadeye@c02850f1-063f-0410-81ad-53bb62183951>
2008-04-06 22:38:42 +0000

    Added base graphic.

--- Final Boom/src/com/anvil/android/boom/particles/SpriteInstance.java
+++ Final Boom/src/com/anvil/android/boom/particles/SpriteInstance.java
@@ -6,24 +6,55 @@ import android.graphics.RectF;
 public class SpriteInstance {
 	public final Bitmap sprite;
 	private RectF drawBox;
+	private RectF originalBox;
 	public final float width;
 	public final float height;
-	private float scale;
+	private float scale = 1.0f;
+	private float aspectRatio = 1.0f;
 	
 	public SpriteInstance(Bitmap sprite) {
 		this.sprite = sprite;
-		this.width = (float)sprite.getWidth();
-		this.height = (float)sprite.getHeight();
-		this.drawBox = new RectF(-(width/2), -(height/2), width/2, height/2);
-		this.scale = 1.0f;
+		width = (float)sprite.getWidth();
+		height = (float)sprite.getHeight();
+		drawBox = new RectF(-(width/2), -(height/2), width/2, height/2);
+		originalBox = this.drawBox;
+
+		if (width > height) {
+			aspectRatio = width/height;
+		}
+		else if (width < height) {
+			aspectRatio = height/width;
+		}
 	}
 
 	public void setScale(float scale) {
 		this.scale = scale;
-		this.drawBox.right *= scale;
-		this.drawBox.left *= scale;
-		this.drawBox.top *= scale;
-		this.drawBox.bottom *= scale;
+		drawBox = originalBox;
+		drawBox.right *= scale;
+		drawBox.left *= scale;
+		drawBox.top *= scale;
+		drawBox.bottom *= scale;
+	}
+	
+	public void setRadius(float radius) {
+		if (width > height) {
+			drawBox.left = -radius;
+			drawBox.right = radius;
+			drawBox.top = -(radius/aspectRatio);
+			drawBox.bottom = radius/aspectRatio;
+		}
+		else if (width < height) {
+			drawBox.top = -radius;
+			drawBox.bottom = radius;
+			drawBox.left = -(radius/aspectRatio);
+			drawBox.right = radius/aspectRatio;
+		}
+		else {
+			drawBox.top = -radius;
+			drawBox.bottom = radius;
+			drawBox.left = -radius;
+			drawBox.right = radius;			
+		}
 	}
 	
 	public float getScale() {
