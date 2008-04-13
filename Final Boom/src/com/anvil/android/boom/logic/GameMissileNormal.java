f5f6740d9b1a3788c06ec4b260b04fce83335880
ksu.deadeye <ksu.deadeye@c02850f1-063f-0410-81ad-53bb62183951>
2008-04-13 22:49:29 +0000

    Fixed smoke emitter to draw the correct direction.

--- Final Boom/src/com/anvil/android/boom/logic/GameMissileNormal.java
+++ Final Boom/src/com/anvil/android/boom/logic/GameMissileNormal.java
@@ -3,6 +3,7 @@ package com.anvil.android.boom.logic;
 import com.anvil.android.boom.graphics.SpriteData;
 import com.anvil.android.boom.graphics.SpriteInstance;
 import com.anvil.android.boom.particles.SmokeEmitter2D;
+import com.anvil.android.boom.logic.Physics;
 
 import android.graphics.Canvas;
 import android.graphics.Paint;
@@ -37,6 +38,8 @@ public class GameMissileNormal extends GameMissile {
 		//TODO: Need to change so we're not accessing a specific index in the sprite array
 		mSmokeEmitter = new SmokeEmitter2D (DEFAULT_NUM_SMOKE_PARTICLES,
 				startingX, startingY, SpriteData.sprites[SpriteData.SMOKE_CLOUD]);
+		mSmokeEmitter.angle = drawAngle;
+		smokeOffset = Physics.scale(mVelocityVector, -12.0f);
 		
 		mExplosionDamage = DEFAULT_NORMAL_MISSILE_DAMAGE;
 		
@@ -49,8 +52,7 @@ public class GameMissileNormal extends GameMissile {
 			mSprite = new SpriteInstance(SpriteData.sprites[SpriteData.STD_MISSILE]);
 		}
 		
-		mSprite.setScale(0.20f);
-		mSprite.setRadius (12);
+		mSprite.setRadius (12.0f);
 	}
 	
 	@Override
@@ -73,14 +75,15 @@ public class GameMissileNormal extends GameMissile {
 			{
 				RectF tempBox = mSprite.getDrawBox();
 				
+				mSmokeEmitter.update (timeElapsed);
+				mSmokeEmitter.draw (canvas);
+				
 				canvas.save();
 				canvas.translate(mCurrentPos.x, mCurrentPos.y);
 				canvas.rotate(drawAngle);				
 		    	canvas.drawBitmap(mSprite.sprite, null, tempBox, paint);
 				canvas.restore();
 				
-				mSmokeEmitter.update (timeElapsed);
-				mSmokeEmitter.draw (canvas);
 			}
 			break;
 			
