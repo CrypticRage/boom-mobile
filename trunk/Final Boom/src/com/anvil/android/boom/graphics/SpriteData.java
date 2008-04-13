fb86330f5892a9f05c146f18a03a63e14aed4f43
ksu.deadeye <ksu.deadeye@c02850f1-063f-0410-81ad-53bb62183951>
2008-04-13 23:39:24 +0000

    Added blast sprite.

--- trunk/Final Boom/src/com/anvil/android/boom/graphics/SpriteData.java
+++ trunk/Final Boom/src/com/anvil/android/boom/graphics/SpriteData.java
@@ -9,11 +9,12 @@ public class SpriteData {
 	public static final int BG_MOUNTAINS = 1;
 	
 	public static volatile Bitmap[] sprites;
-	public static final int spriteCount = 6;
+	public static final int spriteCount = 7;
 	public static final int SMOKE_CLOUD = 0;
 	public static final int STD_MISSILE = 1;
 	public static final int SMART_BOMB = 2;
 	public static final int GOLD_MISSILE = 3;
 	public static final int DOME_BASE = 4;
 	public static final int RED_CROSS = 5;
+	public static final int BLAST = 6;
 }
